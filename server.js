89ca5fcd34eadb3ac59138540a9806a8c1893d58
BraianPerez97 <3603@holbertonstudents.com>
2023-09-07 22:08:20 -0400

    troubleshooting

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 var mysql = require('mysql');
 const inputCheck = require('./utils/inputCheck');
 
+// Model
+const User = require('./models/User'); 
+
 // Port designation
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -22,7 +25,14 @@ var db = mysql.createConnection(
     console.log(`Database connection sucessful.`)
 )
 
+// Parse JSON bodies
+app.use(express.json());
+
+// Connect to database
+const sequelize = require('./config/connection');
+
 // Routes
+
 // Get Categories
 app.get('/api/categories', (req, res) => {
     db.query(`SELECT * FROM categories`, (err, res) => {
@@ -115,34 +125,32 @@ app.post('/api/user', ({body}, res) => {
 });
 
 // UPDATE User
-app.put('/api/user/:id', ({body, params}, res) => {
+app.put('/api/users/:id', (req, res) => {
+    console.log(req);
 
-  const errors = inputCheck(body, 'first_name', 'last_name', 'email', 'passwd');
-
-  if (err) {
-    res.status(400).json({error: err.message});
-    return;
-  }
-
-  const db_call = `UPDATE user SET first_name=?, last_name=?, email=?, passwd=? WHERE id=?`;
-  const params = [body.first_name, body.last_name, body.email, body.passwd, params.id];
-
-  db.query(sdb_call, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    if (result.affectedRows === 0) {
-      res.status(404).json({ error: 'User not found' });
-    } else {
-      res.json({
-        message: 'success',
-        data: body
+    // Get ID from request parameters
+    const id = req.params.id;
+  
+    // Get new user data from request body
+    const updatedUserData = req.body;
+  
+    User.update(updatedUserData, { 
+      where: {
+        id: id
+      }
+    })
+      .then(updatedUser => {
+        if (!updatedUser) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(updatedUser);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: 'Error updating user', error: err });
       });
-    }
+  
   });
-
-});
 
 // Error handling
 app.use((req, res) => {
