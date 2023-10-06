4404dbec6dccb943a45a17b5f030b090c4a75cb1
Nashalys Fernandez <nashalysf@gmail.com>
2023-10-06 19:35:18 -0400

    fixing login route; adding logout and express session for cookies

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const session = require("express-session");
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: 'thisismysecret',
+  cookie: {},
+  resave: false,
+  saveUninitialize: true,
+  store : new SequelizeStore({
+    db: sequelize
+  })
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
